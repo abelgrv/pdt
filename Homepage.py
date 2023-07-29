ef3dc93fabb6ccccada019b7c466d4243ba4d003
abelgrv <140859095+abelgrv@users.noreply.github.com>
2023-07-28 23:51:38 -0500

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -23,6 +23,6 @@ from streamlit_gsheets import GSheetsConnection
 st.title("Read Google Sheet as DataFrame")
 
 conn = st.experimental_connection("gsheets", type=GSheetsConnection)
-df = conn.read(worksheet="Hoja 1")
+df = conn.read(worksheet="Example 1")
 
 st.dataframe(df)
