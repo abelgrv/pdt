018c32ae594e1813ad954969cfc166f20fe51995
abelgrv <140859095+abelgrv@users.noreply.github.com>
2023-07-30 15:03:38 -0500

    Update Projects.py

--- pages/Projects.py
+++ pages/Projects.py
@@ -3,3 +3,6 @@ import streamlit as st
 def Projects_content():
   st.title("Projects")
   st.write("You have entered", st.session_state["my_input"])
+
+st.title("Projects")
+st.write("You have entered", st.session_state["my_input"])
