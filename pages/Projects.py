d139b21f53738c01a9d9fa8bbed25bbddde04dfb
abelgrv <140859095+abelgrv@users.noreply.github.com>
2023-07-30 18:02:43 -0500

    Update Projects.py

--- pages/Projects.py
+++ pages/Projects.py
@@ -34,7 +34,7 @@ def Projects_content():
     search_string = my_input
 
     # Create a boolean mask to identify rows containing the search_string in 'City' column
-    mask = (df['a'] == search_string) | (df['b'] == search_string)
+    mask = (df['a'] == search_string) | (df['b'] == search_string) | (df['c'] == search_string)
 
     # Filter the DataFrame to get rows where 'City' contains the search_string
     filtered_df = df[mask]
