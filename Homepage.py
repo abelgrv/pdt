fc1ae76e1328f70d3d73dcd4e6af98ea2a54953b
abelgrv <140859095+abelgrv@users.noreply.github.com>
2023-07-30 17:18:45 -0500

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -17,8 +17,6 @@ image = Image.open('images/logo.png')
 st.sidebar.image(image)
 st.sidebar.markdown("***")
 
-if "my_input" not in st.session_state:
-    st.session_state["my_input"] = ""	
 consult = st.button("Consulta de Pacientes IREN")
 if consult:
     Projects.Projects_content()
