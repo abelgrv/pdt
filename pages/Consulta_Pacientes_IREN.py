df555b2e4f8c117057aff98acc83f377895c443e
abelgrv <140859095+abelgrv@users.noreply.github.com>
2023-07-30 23:32:18 -0500

    Update Consulta_Pacientes_IREN.py

--- pages/Consulta_Pacientes_IREN.py
+++ pages/Consulta_Pacientes_IREN.py
@@ -79,7 +79,7 @@ def page_content():
     # Display the concatenated DataFrame
     if my_input:
         st.dataframe(concatenated_df)
-        st.write(df_hlinks.to_markdown(escape=False), unsafe_allow_html=True)
+        st.dataframe(df_hlinks.style, unsafe_allow_html=True)
         #st.write(df_hlinks, unsafe_allow_html=True)
         #st.dataframe(concatenated_df1)
   
