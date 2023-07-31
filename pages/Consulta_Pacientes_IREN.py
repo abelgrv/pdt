bc51879fa32684531594813fa3d2200466fd5ad9
abelgrv <140859095+abelgrv@users.noreply.github.com>
2023-07-30 23:19:07 -0500

    Update Consulta_Pacientes_IREN.py

--- pages/Consulta_Pacientes_IREN.py
+++ pages/Consulta_Pacientes_IREN.py
@@ -79,7 +79,7 @@ def page_content():
     # Display the concatenated DataFrame
     if my_input:
         st.dataframe(concatenated_df)
-        st.dataframe(df_hlinks, unsafe_allow_html=True)
+        st.write(df_hlinks, unsafe_allow_html=True)
         #st.dataframe(concatenated_df1)
   
 page_content()
